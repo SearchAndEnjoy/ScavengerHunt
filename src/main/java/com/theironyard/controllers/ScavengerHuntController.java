a382385c05e9917eeebdecf08e4f4afa39a07fae
Erik <eschneider1128@gmail.com>
2016-07-21 09:52:36 -0400

    tsv is parsing

--- src/main/java/com/theironyard/controllers/ScavengerHuntController.java
+++ src/main/java/com/theironyard/controllers/ScavengerHuntController.java
@@ -1,5 +1,6 @@
 package com.theironyard.controllers;
 
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
 import com.theironyard.entities.Clue;
 import com.theironyard.entities.Game;
 import com.theironyard.entities.Team;
@@ -7,11 +8,13 @@ import com.theironyard.services.AnswerRepository;
 import com.theironyard.services.ClueRepository;
 import com.theironyard.services.GameRepository;
 import com.theironyard.services.TeamRepository;
+import org.apache.catalina.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -23,32 +26,40 @@ import java.util.Scanner;
 @RestController
 public class ScavengerHuntController {
 
-    @Autowired
-    TeamRepository teams;
-
-    @Autowired
-    GameRepository games;
+//    @Autowired
+//    TeamRepository teams;
+//
+//    @Autowired
+//    GameRepository games;
 
     @Autowired
     ClueRepository clues;
 
-    @Autowired
-    AnswerRepository answers;
+//    @Autowired
+//    AnswerRepository answers;
 
-    @RequestMapping(path = "/newGame", method = RequestMethod.POST)
-    public Game newGame(String lobbyName, String lobbyCode, HttpSession session) {
-        session.getAttribute()
+    @PostConstruct
+    public void init() throws FileNotFoundException {
+        parseClues("clues.tsv");
     }
 
-    public void readClues() throws FileNotFoundException {
-        File clueFile = new File("clues.tsv");
-        Scanner fileScanner = new Scanner(clueFile);
-        fileScanner.nextLine();
-        while (fileScanner.hasNext()) {
-            String line = fileScanner.nextLine();
-            String [] columns = line.split("\t");
-            Clue clue = new Clue(columns[0], columns[1], Double.valueOf(columns[2]), Double.valueOf(columns[3]));
-            clues.save(clue);
+
+
+//    @RequestMapping(path = "/newGame", method = RequestMethod.POST)
+//    public Game newGame(String lobbyName, String lobbyCode, HttpSession session) {
+//        session.getAttribute()
+//    }
+
+    public void parseClues(String fileName) throws FileNotFoundException {
+        if (clues.count() == 0) {
+            File clueFile = new File(fileName);
+            Scanner fileScanner = new Scanner(clueFile);
+            fileScanner.nextLine();
+            while (fileScanner.hasNext()) {
+                String[] columns = fileScanner.nextLine().split("\t");
+                Clue clue = new Clue(columns[0], columns[1], Double.valueOf(columns[2]), Double.valueOf(columns[3]));
+                clues.save(clue);
+            }
         }
     }
 }
