ec3b7f75b0181255243adc1b6f855e0960c88a9c
geomaas <geoff.maas@gmail.com>
2016-07-21 12:56:19 -0400

    fixed es6 issue with safari browsers

--- Public/main.js
+++ Public/main.js
@@ -20,54 +20,44 @@ module.exports = function(app){
 }
 
 },{}],3:[function(require,module,exports){
-let app = angular.module('HuntApp', ['ngRoute','uiGmapgoogle-maps']);
+'use strict';
+
+var app = angular.module('HuntApp', ['ngRoute', 'uiGmapgoogle-maps']);
 // Controllers
 require('./Controllers/questioncontroller.js')(app);
 require('./Controllers/infocontroller.js')(app);
 
-
-app.config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-        .when('/', {
-            redirectTo: '/start',
-        })
-        .when('/start', {
-            // controller: 'startcontroller',
-            templateUrl: 'templates/start_page.html',
-            controller:'InfoController'
-        })
-        .when('/info1', {
-            templateUrl: 'templates/howtoplay1.html',
-            controller:'InfoController',
-        })
-        .when('/info2', {
-            templateUrl: 'templates/howtoplay2.html',
-            controller: 'InfoController',
-        })
-        .when('/create', {
-            // controller: 'newcontroller',
-            templateUrl: 'templates/newsession.html',
-        })
-        .when('/join', {
-            // controller: 'joincontroller',
-            templateUrl:'templates/joinsession.html',
-        })
-        .when('/lobby', {
-          // controller:'lobbycontroller',
-          templateUrl:'templates/lobby.html',
-        })
-        .when('/list',{
-          // controller:'listcontroller',
-          templateUrl:'templates/questionlist.html',
-        })
-        .when('/question',{
-          controller:'QuestionController',
-          templateUrl:'templates/questionpage.html',
-        })
-        .when('/gameover',{
-          // controller:'gameovercontroller',
-          templateUrl:'templates/gameover.html',
-        });
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/', {
+        redirectTo: '/start'
+    }).when('/start', {
+        // controller: 'startcontroller',
+        templateUrl: 'templates/start_page.html',
+        controller: 'InfoController'
+    }).when('/info1', {
+        templateUrl: 'templates/howtoplay1.html',
+        controller: 'InfoController'
+    }).when('/info2', {
+        templateUrl: 'templates/howtoplay2.html',
+        controller: 'InfoController'
+    }).when('/create', {
+        // controller: 'newcontroller',
+        templateUrl: 'templates/newsession.html'
+    }).when('/join', {
+        // controller: 'joincontroller',
+        templateUrl: 'templates/joinsession.html'
+    }).when('/lobby', {
+        // controller:'lobbycontroller',
+        templateUrl: 'templates/lobby.html'
+    }).when('/list', {
+        // controller:'listcontroller',
+        templateUrl: 'templates/questionlist.html'
+    }).when('/question', {
+        controller: 'QuestionController',
+        templateUrl: 'templates/questionpage.html'
+    }).when('/gameover', {
+        // controller:'gameovercontroller',
+        templateUrl: 'templates/gameover.html'
+    });
 }]);
-
 },{"./Controllers/infocontroller.js":1,"./Controllers/questioncontroller.js":2}]},{},[3])
