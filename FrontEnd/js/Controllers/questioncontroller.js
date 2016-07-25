19de12feecb1021933f6ddd9244e400bfc40f645
geomaas <geoff.maas@gmail.com>
2016-07-24 22:33:47 -0400

    added haversine formula

--- FrontEnd/js/Controllers/questioncontroller.js
+++ FrontEnd/js/Controllers/questioncontroller.js
@@ -1,10 +1,40 @@
-module.exports = function(app){
-  app.controller('QuestionController',['$scope','$http','MainService',function($scope,$http,MainService){
+module.exports = function(app) {
+    app.controller('QuestionController', ['$scope', '$http', 'MainService', function($scope, $http, MainService) {
 
-    $scope.myLoc = MainService.getLocation()
-    $scope.marker = function(){
-      console.log($scope.myLoc)
-      MainService.CreateMarker()
-    }
-}])
+        $scope.myLoc = MainService.getLocation()
+
+        $scope.marker = function() {
+            console.log($scope.myLoc)
+            function distance(lon1, lat1, lon2, lat2) {
+                var R = 6371; // Radius of the earth in km
+                var dLat = (lat2 - lat1).toRad(); // Javascript functions in radians
+                var dLon = (lon2 - lon1).toRad();
+                var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                    Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+                    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+                var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+                var d = R * c; // Distance in km
+                return d;
+            }
+
+            /** Converts numeric degrees to radians */
+            if (typeof(Number.prototype.toRad) === "undefined") {
+                Number.prototype.toRad = function() {
+                    return this * Math.PI / 180;
+                }
+            }
+
+
+                console.log($scope.myLoc);
+                console.log(
+                    $scope.myLoc[0].lat, $scope.myLoc[0].lon, 32.7785834,-79.9378045
+                );
+                console.log(
+                    distance($scope.myLoc[0].lat, $scope.myLoc[0].lon, 32.7785834,-79.9378045)
+                );
+            MainService.CreateMarker();
+        };
+
+
+    }])
 }
