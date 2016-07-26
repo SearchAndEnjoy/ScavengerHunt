e5655724fb6a6f33f0deca4d78deec9ba10a001b
geomaas <geoff.maas@gmail.com>
2016-07-26 10:24:44 -0400

    flipclock counting down

--- FrontEnd/js/Controllers/listcontroller.js
+++ FrontEnd/js/Controllers/listcontroller.js
@@ -6,43 +6,37 @@ module.exports = function(app) {
         console.log('clicked');
       };
 
-      $scope.init = function () {
 
-        };
-        // var clock = $('.clock').FlipClock(3600, {
-        //     autoStart: true,
-        //     countdown: true
-        // });
+////// function courtesy of http://questionandanswer.website/question/31670979-flipclock-js-countdown-1hour-without-reset
+////// flipclock courtesy of flipclockjs.com
+
         $(function(){
 
             countDown = function(){
                 var currentDate = Math.round(new Date() / 1000);
 
                 var clock = $('.clock').FlipClock({
-                  clockFace: 'HourlyCounter',
                     countdown: true,
                     callbacks: {
                         init: function() {
                             //store end date If it's not yet in cookies
                             if(!$.cookie('endDate')){
-                                // end date = current date + 1 minutes
+                                // end date = current date + 60 minutes
                                 var endDate = Date.now() + 60*60*1000;
 
                                 // store end date in cookies
                                 $.cookie('endDate', Math.round(endDate / 1000));
                             }
                         },
-                        stop: function() {
-                            $('.message').html('The clock has stopped!');
-                        },
+
                     }
                 });
-
+                console.log($.cookie('endDate'));
                 /* counter will be at first 1 min if the user refresh the page the counter will
                    be the difference between current and end Date, so like this counter can
                    continue the countdown normally in case of refresh. */
                 var counter = $.cookie('endDate')-currentDate;
-
+                //
                 clock.setTime(counter);
                 clock.setCountdown(true);
 
